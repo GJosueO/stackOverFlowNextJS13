1b574458efd5d0832fc5002869233323b7315ff7
Josue <josuegzariett@gmail.com>
2024-03-15 08:26:31 -0600

    Navbar & header first part

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,11 +1,11 @@
-import { UserButton } from '@clerk/nextjs'
+
 import React from 'react'
 
 const Home = () => {
   return (
     <div className='flex min-h-screen w-full items-center justify-center '>
       Home
-      <UserButton />
+      
     </div>
   )
 }
